885fe8af6781a054df5048ab655efacb22b2a45f
sayali2004 <93052981+sayali2004@users.noreply.github.com>
2025-04-05 00:43:52 +0530

    Update categories.dart

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+//import 'package:flutter/rendering.dart';
 import 'package:meal_app/data/dummy_data.dart';
 import 'package:meal_app/models/meal.dart';
 import 'package:meal_app/screens/meals.dart';
